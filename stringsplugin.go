2c056391f8a3076b7dae015cdb90f8051eeb610e
Steven <steven@blueboxonline.com>
2025-11-28 13:15:14 +0200

    Provision required for certain vars

--- stringsplugin.go
+++ stringsplugin.go
@@ -19,6 +19,7 @@ func init() {
 // Interface guards
 var (
 	_ caddy.Module                = (*Strings)(nil)
+	_ caddy.Provisioner           = (*Strings)(nil)
 	_ caddyhttp.MiddlewareHandler = (*Strings)(nil)
 	_ caddyfile.Unmarshaler       = (*Strings)(nil)
 )
@@ -33,42 +34,56 @@ func (Strings) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
+// Provision registers a global mapping for placeholders
+func (m *Strings) Provision(ctx caddy.Context) error {
+    // global mapping for .upper/.lower
+    repl := caddy.NewReplacer()
+    mapStringCases(repl)
+
+    return nil
+}
+
 func (m *Strings) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
 	repl, ok := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
 	if ok {
-		repl.Map(func(key string) (any, bool) {
-			base := key
-			lower := false
-			upper := false
-
-			if strings.HasSuffix(key, ".lower") || strings.HasSuffix(key, ".upper") {
-				base = strings.TrimSuffix(key, ".lower")
-				lower = true
-			} else if strings.HasSuffix(key, ".upper") {
-				base = strings.TrimSuffix(key, ".upper")
-				upper = true
-			}
-
-			if (lower || upper) {
-				val, found := repl.GetString(base)
-				if !found {
-					return nil, false
-				}
-
-				if lower {
-					val = strings.ToLower(val)
-				} else if upper {
-					val = strings.ToUpper(val)
-				}
-
-				return val, true
-			}
-			
-			return nil, false
-		})
+		mapStringCases(repl)
 	}
 
 	return next.ServeHTTP(w, r)
+}
+
+//function that takes repl and maps .lower and .upper
+func mapStringCases(repl *caddy.Replacer) {
+	repl.Map(func(key string) (any, bool) {
+		base := key
+		lower := false
+		upper := false
+
+		if strings.HasSuffix(key, ".lower") || strings.HasSuffix(key, ".upper") {
+			base = strings.TrimSuffix(key, ".lower")
+			lower = true
+		} else if strings.HasSuffix(key, ".upper") {
+			base = strings.TrimSuffix(key, ".upper")
+			upper = true
+		}
+
+		if (lower || upper) {
+			val, found := repl.GetString(base)
+			if !found {
+				return nil, false
+			}
+
+			if lower {
+				val = strings.ToLower(val)
+			} else if upper {
+				val = strings.ToUpper(val)
+			}
+
+			return val, true
+		}
+		
+		return nil, false
+	})
 }
 
 // UnmarshalCaddyfile reads static options (none for now)
